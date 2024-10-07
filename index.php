253eae002293174ec353817c3c7842668b0c7f58
Ka-atsu <tallaferkent775@gmail.com>
2024-10-07 09:02:55 +0800

    BUCKET2

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
     <div id="overlay"></div>
 
     <header class="navbar">
-        <h1>BUCKET</h1>
+        <h1>BUCKET </h1>
         <div class="hamburger" id="hamburger">
             <div class="hamburger-content">
                 <span></span>
